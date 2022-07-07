8f4d02acf72b6426d41a7979d397dffd0fb78a24
mrneont <neon.taylor@gmail.com>
2022-07-07 12:12:55 -0400

    add mahalanobis dist calc, though not really using

--- demo_point-cloud.py
+++ demo_point-cloud.py
@@ -29,7 +29,9 @@ if __name__ == "__main__" :
     # get fundamental properties of point-cloud:
     #   mu       = the coordinate of the mean of the point-cloud
     #   eig_axes = the slopes/axes of the point-cloud
-    mu, eig_axes, U, S = lpc.calc_slope_int_pts(pts)
+    mu, eig_axes, cov, U, S = lpc.calc_slope_int_pts(pts)
+
+    mdist = lpc.calc_mahalanobis_dist(pts, mu, cov)
 
     # see if there are any outliers
     #   pts_in   = point-cloud of non-outliers (must be non-empty)
@@ -47,13 +49,13 @@ if __name__ == "__main__" :
     if Nin :
         # first need to calc mean+axes of this new point-cloud (well,
         # could be same as original)
-        mu_in, eig_axes_in, U, S = lpc.calc_slope_int_pts(pts_in)
+        mu_in, eig_axes_in, cov, U, S = lpc.calc_slope_int_pts(pts_in)
         pts_in0, pts_in_bnd0 = lpc.calc_bnds_along_line(pts_in, mu_in, eig_axes_in, 0)
         pts_in1, pts_in_bnd1 = lpc.calc_bnds_along_line(pts_in, mu_in, eig_axes_in, 1)
         pts_in2, pts_in_bnd2 = lpc.calc_bnds_along_line(pts_in, mu_in, eig_axes_in, 2)
     if Nout :
         # first need to calc mean+axes of this new point-cloud
-        mu_out, eig_axes_out, U, S = lpc.calc_slope_int_pts(pts_out)
+        mu_out, eig_axes_out, cov, U, S = lpc.calc_slope_int_pts(pts_out)
         pts_out0, pts_out_bnd0 = lpc.calc_bnds_along_line(pts_out, mu_out, eig_axes_out, 0)
         pts_out1, pts_out_bnd1 = lpc.calc_bnds_along_line(pts_out, mu_out, eig_axes_out, 1)
         pts_out2, pts_out_bnd2 = lpc.calc_bnds_along_line(pts_out, mu_out, eig_axes_out, 2)
@@ -74,6 +76,8 @@ if __name__ == "__main__" :
     ax.plot3D(*pts1.T, c='C4', ls = '--')
     ax.plot3D(*pts2.T, c='C4', ls = '--', label='all pts')
 
+    #ax.scatter(*pts.T, c='k', s=10*mdist, label='pts_all')
+    
     # IN: lines and points
     if Nin :
         ax.plot3D(*pts_in0.T, c='C3')
@@ -87,7 +91,7 @@ if __name__ == "__main__" :
         ax.plot3D(*pts_out1.T, c='C0')
         ax.plot3D(*pts_out2.T, c='C0')
         ax.scatter3D(*pts_out.T, c='C0', label='pts_out')
-
+    
     plt.ion()
     plt.legend()
     plt.show()
